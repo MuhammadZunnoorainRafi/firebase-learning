73b59a1db69b67c9830720c4475a9c180c99a572
Zunnoorain Rafi <zunnoorainrafi236@gmail.com>
2024-09-19 15:47:52 +0500

    load more functionality added

--- src/components/post/PostList.tsx
+++ src/components/post/PostList.tsx
@@ -1,22 +1,62 @@
 import { db } from '@/firebase.config';
 import { Post } from '@/utils/types';
-import { collection, getDocs, orderBy, query } from 'firebase/firestore';
+import {
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  startAfter,
+} from 'firebase/firestore';
 import { useEffect, useState } from 'react';
 import PostForm from './PostForm';
 import PostItem from './PostItem';
+import { Button } from '../ui/button';
 
 function PostList() {
   const [posts, setPosts] = useState<Post[]>([]);
-
+  const [isEmpty, setIsEmpty] = useState(false);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [lastFetchedPost, setLastFetchedPost] = useState<any | null>(null);
+  console.log(lastFetchedPost);
   const fetchAllPosts = async () => {
     try {
-      const q = query(collection(db, 'posts'), orderBy('timestamp', 'desc'));
+      const q = query(
+        collection(db, 'posts'),
+        orderBy('timestamp', 'desc'),
+        limit(3)
+      );
       const allPosts = await getDocs(q);
+      const lastVisible = allPosts.docs[allPosts.docs.length - 1];
+      setLastFetchedPost(lastVisible);
       const postsData = allPosts.docs.map((val) => ({
         id: val.id,
         ...val.data(),
       })) as Post[];
+      setIsEmpty(allPosts.size < 3);
       setPosts(postsData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchOnLoadMore = async () => {
+    try {
+      const q = query(
+        collection(db, 'posts'),
+        orderBy('timestamp', 'desc'),
+        startAfter(lastFetchedPost),
+        limit(3)
+      );
+      const allPosts = await getDocs(q);
+      const lastVisible = allPosts.docs[allPosts.docs.length - 1];
+      setLastFetchedPost(lastVisible);
+      const postsData = allPosts.docs.map((val) => ({
+        id: val.id,
+        ...val.data(),
+      })) as Post[];
+      setIsEmpty(allPosts.size < 3);
+      setPosts((prev) => [...prev, ...postsData]);
     } catch (error) {
       console.log(error);
     }
@@ -33,6 +73,9 @@ function PostList() {
       {posts.map((post) => (
         <PostItem key={post.id} post={post} fetchAllPosts={fetchAllPosts} />
       ))}
+      <div className="text-center">
+        {!isEmpty && <Button onClick={fetchOnLoadMore}>Load more</Button>}
+      </div>
     </div>
   );
 }
